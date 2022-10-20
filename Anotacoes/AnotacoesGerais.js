dd898f966848c3994f0d31e662e70b45624f3dbd
Lucas Nunes o/ <lucasrafaelnunees@gmail.com>
2022-10-20 01:43:55 -0300

    Exercios e Variaveis Let,const e Var

--- Anotacoes/AnotacoesGerais.js
+++ Anotacoes/AnotacoesGerais.js
@@ -1,5 +1,7 @@
 
-// Mostrar algo na tela:
+    // Mostrar algo na tela: //
+
+/*
 console.log("Exemplo de texto.. poderia ser um objeto ou variavel."); //Tipo String = Texto
 console.log('Diferença para utilizar aspas "Se eu quero esse texto em aspas" eu preciso abrir um aspas simples e vice-versa'); //Tipo String = Texto
 console.log(`Outro exemplo de aspas, 'desta forma eu posso utilizar' as "duas aspas"` ); //Tipo String = Texto
@@ -8,12 +10,21 @@ console.log(123456); //Tipo INT
 
 console.log('Meu nome é: "Lucas Nunes". Estou aprendendo JavaScript às', 10,'da manhã.' ); 
 
-// Repositorio: https://github.com/luizomf/curso-js
+*/
+
+    // Repositorio: https://github.com/luizomf/curso-js //
 
 
-// Comentários.
+    // Comentários. //
 /*   
 
     Bloco de comentário.
 
 */
+
+// String = text
+// Number = Número
+
+    //Para verificar o tipo de dado, qual o tipo da minha váriavel se é int, string etc
+//console.log(typeof(primeiroNumero))
+
