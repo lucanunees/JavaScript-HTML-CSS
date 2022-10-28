a0d9e349867b9bd175c4a29b0fcb1956c0ab0412
Lucas Nunes o/ <lucasrafaelnunees@gmail.com>
2022-10-28 16:01:39 -0300

    Exercicios com String com HTML

--- alertConfirmEPrompt/JS/script.js
+++ alertConfirmEPrompt/JS/script.js
@@ -13,19 +13,3 @@
 //let idade = prompt('Digite sua idade:');
 
 
-    // EXERCICIO //
-    // FAÇA UM CALCULO ATRAVÉS DO NAVEGADOR//
-
-alert('Calculadora Online, clicar em OK para iniciar.');
-let num1 = prompt('Digite um número:');
-let num2 = prompt('Para efetuar a soma, digite outro número:')
-
-num1 = Number(num1);
-num2 = Number(num2);
-
-
-let resultadoDaSoma = num1 + num2;
-
-alert(`O resultado da soma dos valores digitados é de: ${resultadoDaSoma}`);
-
-
